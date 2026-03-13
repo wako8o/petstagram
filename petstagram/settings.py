21fdb0c5cebe5801e698abaf5870290d489b0422
wako8o <emilaleksandrov11@gmail.com>
2026-03-13 12:23:12 +0100

    now

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -6,13 +6,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-hb+y!5a^cg=e-5fa_f-o@xx+g#*3&-2$-*7rl831+u&8u7(hke'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-
 
 ]
 
